1ed3eba3b39c638f89d4ede3bc39767019b59a74
Adeyemi Olaoye <yhemi06@gmail.com>
2022-02-09 23:05:43 +0100

    Added HeaderOption Component to HeaderOptions Component

---
+++ components/HeaderOption.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderOption = () => {
+  return (
+    <div>HeaderOption</div>
+  )
+}
+
+export default HeaderOption
--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,7 +1,20 @@
 import React from "react";
+import HeaderOption from "./HeaderOption";
 
 const HeaderOptions = () => {
-  return <div> </div>;
+  return <div> 
+      {/* Left Section */}
+      <div>
+        <div className="flex space-x-6">
+            <HeaderOption />
+        </div>
+      </div>
+      {/* Right Section */}
+      <div className="flex space-x-2">
+          <p className="link">Settings</p>
+          <p className="link">Tools</p>
+      </div>
+  </div>;
 };
 
 export default HeaderOptions;
