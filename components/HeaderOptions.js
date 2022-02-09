1e7b9b22ab899a484a90fa57d0085033ea288ede
Adeyemi Olaoye <yhemi06@gmail.com>
2022-02-09 23:32:18 +0100

    Modified HeaderOption Components for search result page

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,20 +1,35 @@
 import React from "react";
 import HeaderOption from "./HeaderOption";
+import {
+  DotsVerticalIcon,
+  MapIcon,
+  NewspaperIcon,
+  PhotographIcon,
+  PlayIcon,
+  SearchIcon,
+} from "@heroicons/react/outline";
 
 const HeaderOptions = () => {
-  return <div> 
+  return (
+    <div className="flex w-full text-gray-700 justify-evenly text-sm lg:text-base 
+    lg:justify-start lg:space-x-36 lg:pl-52 border-b-[1px]">
       {/* Left Section */}
-      <div>
-        <div className="flex space-x-6">
-            <HeaderOption />
-        </div>
+      <div className="flex space-x-6">
+        <HeaderOption Icon={SearchIcon} title="All" selected />
+        <HeaderOption Icon={PhotographIcon} title="Images" />
+        <HeaderOption Icon={PlayIcon} title="Videos" />
+        <HeaderOption Icon={NewspaperIcon} title="News" />
+        <HeaderOption Icon={MapIcon} title="Maps" />
+        <HeaderOption Icon={DotsVerticalIcon} title="More" />
       </div>
+
       {/* Right Section */}
       <div className="flex space-x-2">
-          <p className="link">Settings</p>
-          <p className="link">Tools</p>
+        <p className="link">Settings</p>
+        <p className="link">Tools</p>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default HeaderOptions;
