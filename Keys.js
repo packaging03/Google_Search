8c43491d9260cee124d972cc35e3076675799c22
Adeyemi Olaoye <yhemi06@gmail.com>
2022-02-10 23:51:08 +0100

    Added environment variable

--- Keys.js
+++
@@ -1,6 +0,0 @@
-//API Key - https://developers/google/com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyBrvyDy358WJSWnKna-4b1V7k3k8hwvG2U";
-
-//Context Key - https://cse.google.com/cse/create/new
-
-export const CONTEXT_KEY = "de7565625b6d7d177";
--- pages/Search.js
+++ pages/Search.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import React from "react";
 import { render } from "react-dom";
 import Header from "../components/Header";
-import { API_KEY, CONTEXT_KEY } from "../Keys";
+// import { API_KEY, CONTEXT_KEY } from "../Keys";
 import Response from "../Response";
 import { useRouter } from "next/router";
 import SearchResults from "../components/SearchResults";
@@ -30,7 +30,7 @@ export async function getServerSideProps(context) {
   const data = useDummyData
     ? Response
     : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
       ).then((response) => response.json());
 
   // After server render, pass result to the client
