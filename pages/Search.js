85bc6a04d604c780e873fc1de5f7ef07f77db527
Adeyemi Olaoye <yhemi06@gmail.com>
2022-02-10 22:01:41 +0100

    Added SearchResults Component to display  data

--- pages/Search.js
+++ pages/Search.js
@@ -3,17 +3,22 @@ import React from "react";
 import { render } from "react-dom";
 import Header from "../components/Header";
 import { API_KEY, CONTEXT_KEY } from "../Keys";
+import Response from "../Response";
+import { useRouter } from "next/router";
+import SearchResults from "../components/SearchResults";
 
 const Search = ({ results }) => {
   console.log(results);
+  const router = useRouter();
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
       </Head>
       {/* Header */}
       <Header />
       {/* Search Result */}
+      <SearchResults results={results} />
     </div>
   );
 };
@@ -21,10 +26,13 @@ const Search = ({ results }) => {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = false;
-  const data = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
-  ).then((response) => response.json());
+  const useDummyData = true;
+  const startIndex = context.query.start || "0";
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
 
   // After server render, pass result to the client
   return {
