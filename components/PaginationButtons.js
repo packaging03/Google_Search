97eed26869260fe55521b081b84d22a6aa32fef7
Adeyemi Olaoye <yhemi06@gmail.com>
2022-02-10 23:17:06 +0100

    Modified pagination component

--- components/PaginationButtons.js
+++ components/PaginationButtons.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React from "react";
+import Link from "next/link";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 const PaginationButtons = () => {
-  return (
-    <div>PaginationButtons</div>
-  )
-}
+  const router = useRouter();
+  const startIndex = Number(router.query.start) || 0;
 
-export default PaginationButtons
+  return (
+    <div className="flex max-w-lg justify-between text-blue-700 mb-10">
+      {startIndex >= 10 && (
+        <Link
+          href={`/Search?term=${router.query.term}&start=${startIndex - 10}`}
+        >
+          <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+            <ChevronLeftIcon className="h-5" />
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}
+
+      <Link href={`/Search?term=${router.query.term}&start=${startIndex + 10}`}>
+        <div className="flex flex-grow flex-col items-center cursor-pointer hover:underline">
+          <ChevronRightIcon className="h-5" />
+          <p>Next</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default PaginationButtons;
